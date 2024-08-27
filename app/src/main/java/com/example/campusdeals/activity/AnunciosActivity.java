b7a9081fff262fd4cbea0df221de25a7a0f4e3c4
= <=>
2024-08-27 14:17:47 -0300

    Login e logout linkados com tela de anuncio

--- app/src/main/java/com/example/campusdeals/activity/AnunciosActivity.java
+++ app/src/main/java/com/example/campusdeals/activity/AnunciosActivity.java
@@ -1,10 +1,13 @@
 package com.example.campusdeals.activity;
 
-import android.content.pm.ConfigurationInfo;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,11 +20,13 @@ import com.google.firebase.auth.FirebaseAuth;
 public class AnunciosActivity extends AppCompatActivity {
     private FirebaseAuth autenticacao;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_anuncios);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -29,12 +34,15 @@ public class AnunciosActivity extends AppCompatActivity {
         });
 
         autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+//        autenticacao.signOut();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+
+
     }
 
     @Override
@@ -46,5 +54,22 @@ public class AnunciosActivity extends AppCompatActivity {
         }
 
         return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.menu_cadastrar){
+            startActivity( new Intent(getApplicationContext(), LoginActivity.class));
+        } else if (item.getItemId() == R.id.menu_sair) {
+            autenticacao.signOut();
+            invalidateOptionsMenu();
+            Toast.makeText(getApplicationContext(),
+                    "Deslogado com sucesso!",
+                    Toast.LENGTH_LONG).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
